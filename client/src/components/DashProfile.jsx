02c85d40e2a7d81996f05f2501aa25b3ffd32a7d
sunny <sunnya2023@gmail.com>
2024-07-11 01:01:26 +0900

    complete profile page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import "../style/component_style/dsahboardProfile.scss";
+import { useSelector } from "react-redux";
 
 export default function DashProfile() {
-  return <div>DashProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="dashProfile">
+      <h1>프로필</h1>
+      <form>
+        <img src={currentUser.profilePictuer} alt={currentUser.username} />
+        <input type="text" value={currentUser.username} />
+        <input type="email" value={currentUser.email} />
+        <input type="password" placeholder="비밀번호" />
+        <button className="edit-btn">수정하기</button>
+      </form>
+      <div className="deleteAccount">
+        <span>탈퇴하기</span>
+      </div>
+    </div>
+  );
 }
